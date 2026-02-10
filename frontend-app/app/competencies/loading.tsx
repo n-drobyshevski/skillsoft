391e2ad196f7bdde213751205e9a7e66ddccd800
nicolai <n.drobyshevski@gmail.com>
2026-02-10 10:55:38 +0100

    Add git submodule

--- frontend-app/app/competencies/loading.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-// import { LoadingSkeleton } from "@/app/competencies/components/loading-skeleton";
-
-export default function Loading() {
-	// Add fallback UI that will be shown while the route is loading.
-	//   return <LoadingSkeleton />;
-}
